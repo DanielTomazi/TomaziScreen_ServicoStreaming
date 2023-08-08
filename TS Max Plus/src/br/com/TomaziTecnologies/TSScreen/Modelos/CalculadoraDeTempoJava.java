afdbf74bf21f5ec1913a23175b5b20431dfaaf95
Daniel Tomazi de Oliveira <125170893+DanielTomazi@users.noreply.github.com>
2023-08-08 16:53:40 -0300

    CalculadoraDeTempoJava.java
    Update referente a remoção de um comentário que foi substituído por um método mais aprimorado.

--- TS Max Plus/src/br/com/TomaziTecnologies/TSScreen/Modelos/CalculadoraDeTempoJava.java
+++ TS Max Plus/src/br/com/TomaziTecnologies/TSScreen/Modelos/CalculadoraDeTempoJava.java
@@ -11,13 +11,7 @@ public class CalculadoraDeTempoJava {
     public void setTempoTotal(int tempoTotal) {
         this.tempoTotal = tempoTotal;
     }
-//    public void inclui(Film f){
-//        this.tempoTotal += f.getDuracaoEmMinutos();
-//    }
-//    public void inclui(Serie s) {
-//        this.tempoTotal += s.getDuracaoEmMinutos();
-//    }
-
+    
     public void inclui(Titulo titulo){
         System.out.println("Adicionando duração em minutos de " + titulo);
         this.tempoTotal = titulo.getDuracaoEmMinutos();
