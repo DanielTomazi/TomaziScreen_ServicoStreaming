319ad1034f9daf5050dac9d67f8764293bd2df54
Daniel Tomazi de Oliveira <125170893+DanielTomazi@users.noreply.github.com>
2023-08-14 03:28:32 -0300

    Update 1.7
    Update 1.7 - 14/08/2023

---
+++ Tomazi Screen/src/br/com/tomazitecnologies/tomaziscreen/calculos/Classificavel.java
@@ -0,0 +1,5 @@
+package br.com.tomazitecnologies.tomaziscreen.calculos;
+
+public interface Classificavel {
+    int getClassificacao();
+}
---
+++ Tomazi Screen/src/br/com/tomazitecnologies/tomaziscreen/modelos/Filme.java
@@ -0,0 +1,19 @@
+package br.com.tomazitecnologies.tomaziscreen.modelos;
+import br.com.tomazitecnologies.tomaziscreen.calculos.Classificavel;
+
+public class Filme extends Titulo implements Classificavel {
+    private String diretor;
+
+    public String getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
+
+    @Override
+    public int getClassificacao() {
+        return (int) pegaMedia() / 2;
+    }
+}
